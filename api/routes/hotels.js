a7dcc4002de5cc0fc0749d874cae4d6f19969034
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-28 17:09:30 +0500

    MERN_BOOKING_APP

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,24 @@
 import express from 'express'
+import Hotel from './models/Hotel.js'
 
 const router = express.Router()
 
+
+// CREATE
+router.post('/', (req, res) => {
+    const newHotel = new Hotel(req.body)
+
+    try {
+        const saveHotel = new newHotel.save()
+        res.status(201).json(saveHotel)
+    } catch (err) {
+        res.status(500).json(err)
+
+    }
+})
+// UPDATE
+// DELETE
+// GET
+// GET ALL
 
 export default router
