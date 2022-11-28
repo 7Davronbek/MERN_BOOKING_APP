43aeba352667d58e868f0271640838aa99fd9c9e
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-28 17:53:30 +0500

    MERN_BOOKING_APP

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,24 +1,70 @@
 import express from 'express'
-import Hotel from './models/Hotel.js'
+import Hotel from '../models/Hotel.js'
 
 const router = express.Router()
 
 
 // CREATE
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const newHotel = new Hotel(req.body)
 
     try {
-        const saveHotel = new newHotel.save()
+        const saveHotel = await newHotel.save()
         res.status(201).json(saveHotel)
     } catch (err) {
         res.status(500).json(err)
 
     }
 })
+
 // UPDATE
+router.put('/:id', async (req, res) => {
+    try {
+        const updateHotel = await Hotel.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        )
+        res.status(200).json(updateHotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 // DELETE
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteHotel = await Hotel.findByIdAndDelete(
+            req.params.id
+        )
+        res.status(200).json('Hotel has been deleted.')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 // GET
+router.get('/:id', async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(
+            req.params.id
+        )
+        res.status(200).json(hotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 // GET ALL
+router.get('/', async (req, res, next) => {
+    try {
+        const hotels = await Hotel.find(
+            req.params.id
+        )
+        res.status(200).json(hotels)
+    } catch (err) {
+        next(err)
+    }
+})
 
 export default router
