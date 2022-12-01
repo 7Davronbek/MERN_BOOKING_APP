d03ae389e28e90042763efa7f7092649b7aed7f6
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-12-01 09:37:41 +0500

    MERN_BOOKING_APP

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,16 @@
 import express from 'express'
 import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.js'
+import { verifyToken, verifyUser } from '../utils/verifyToken.js'
 
 const router = express.Router()
+
+router.get('/checkauthentication', verifyToken, (req, res) => {
+    res.send('You are loggin in')
+})
+
+router.get('/checkuser/:id', verifyUser, (req, res) => {
+    res.send('You are loggin in')
+})
 
 // UPDATE
 router.put('/:id', updateUser)
