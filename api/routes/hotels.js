af28e916ef1af4f7390bdd704986e7c0b23484b0
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-12-02 11:14:55 +0500

    MERN_BOOKING_APP

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js'
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js'
 import { verifyAdmin } from '../utils/verifyToken.js'
 
 const router = express.Router()
@@ -12,12 +12,14 @@ router.post('/', verifyAdmin, createHotel)
 router.put('/:id', verifyAdmin, updateHotel)
 
 // DELETE
-router.delete('/:id', verifyAdmin, deleteHotel)
+router.delete('/find/:id', verifyAdmin, deleteHotel)
 
 // GET
-router.get('/:id', getHotel)
+// router.get('/:id', getHotel)
 
 // GET ALL
 router.get('/', getHotels)
+router.get('/countByCity', countByCity)
+router.get('/countByType', getHotels)
 
 export default router
