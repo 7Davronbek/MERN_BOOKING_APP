c27046ecf100e1e99aad54c78f659f45a2b804ba
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-12-02 18:01:20 +0500

    MERN_BOOKING_APP

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,21 +1,36 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
 
+const [data, setData] = useState([])
+const [loading, setLoading] = useState(false)
+const [error, setError] = useState(false)
 
 const useFetch = url => {
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(false)
-}
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true)
 
-useEffect(() => {
-    const fetchData = async () => {
+            try {
+                const res = await axios.get(url)
+                setData(res.data)
+            } catch (error) {
+                setError(error)
+            }
+        }
+
+        fetchData()
+    }, [url])
+
+    const reFetch = async () => {
         setLoading(true)
 
         try {
-            const res = await axios
+            const res = await axios.get(url)
+            setData(res.data)
         } catch (error) {
             setError(error)
         }
+
+        return { data, loading, error, reFetch }
     }
-}, [])
+}
