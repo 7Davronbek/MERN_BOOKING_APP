cbc5a49f2a49bfcb598d640c74d35665e753bad9
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-12-02 11:34:48 +0500

    MERN_BOOKING_APP

---
+++ client/src/hooks/useFetch.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+
+
+const useFetch = url => {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+}
+
+useEffect(() => {
+    const fetchData = async () => {
+        setLoading(true)
+
+        try {
+            const res = await axios
+        } catch (error) {
+            setError(error)
+        }
+    }
+}, [])
